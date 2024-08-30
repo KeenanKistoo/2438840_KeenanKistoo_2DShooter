392b1122da072fc2dbd567b894dc7fd33fbdba8e
KeenanKistoo <kistook19@gmail.com>
2024-08-31 00:37:45 +0200

    Fixed Camera and Lighting

---
+++ 2DShooter_Games_AI/Assets/player_scripts/ResetPlayer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+
+    public void Reset()
+    {
+        //print("Is working");
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+        _player.transform.position = new Vector3(0, 0, 0);
+        
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+}

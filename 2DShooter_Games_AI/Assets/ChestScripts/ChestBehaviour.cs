ce7499fdcc8d24f76921b52c49052aa5a717a673
KeenanKistoo <kistook19@gmail.com>
2024-09-24 23:41:17 +0200

    Prep For Obj Placement

---
+++ 2DShooter_Games_AI/Assets/ChestScripts/ChestBehaviour.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestBehaviour : MonoBehaviour
+{
+    [Header("Player")]
+    [SerializeField] private GameObject _player;
+    
+    [Header("Animations:")]
+    [SerializeField] private Animation _openAnim;
+    [SerializeField] private Animation _idleAnim;
+
+    [Header("Animator:")] 
+    [SerializeField] private Animator _animator;
+    
+
+    private void Awake()
+    {
+        _player = GameObject.FindWithTag("Player");
+        _animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject == _player)
+        {
+            _animator.SetTrigger("OpenChest");
+        }
+    }
+}

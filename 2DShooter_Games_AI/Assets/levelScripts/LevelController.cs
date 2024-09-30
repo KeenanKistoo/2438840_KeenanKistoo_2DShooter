8596f415704c5bddd37e83b5627567838b785c2e
KeenanKistoo <kistook19@gmail.com>
2024-09-30 16:49:13 +0200

    Game Mechanics Done
    Need to add player animations

--- 2DShooter_Games_AI/Assets/levelScripts/LevelController.cs
+++ 2DShooter_Games_AI/Assets/levelScripts/LevelController.cs
@@ -14,9 +14,13 @@ public class LevelController : MonoBehaviour
     [SerializeField]
     private Loadbar _loadbar;
 
+    [SerializeField] private ItemDistribution _item;
+
     private void Start()
     {
+        
         mainPanel.SetActive(true);
+        _item = GameObject.FindGameObjectWithTag("ItemController").GetComponent<ItemDistribution>();
         tutPanel.SetActive(false);
         loadPane.SetActive(false);
     }
@@ -28,8 +32,32 @@ public class LevelController : MonoBehaviour
         StartCoroutine(StallForGen());
     }
 
+    public void SetChest(int val)
+    {
+        _item.desChest = val;
+    }
+
+    public void SetKey(int val)
+    {
+        Inventory _inventory = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>();
+
+        _inventory.keyLvl = val;
+    }
+
+    public void SetEnemy(int val)
+    {
+        _item.desEnemy = val;
+    }
+
+    public void SetTorch(int val)
+    {
+        _item.desTorch = val;
+
+    }
+
     private IEnumerator StallForGen()
     {
+        
         loadPane.SetActive(true);
         _loadbar.StartLoad(10f);
         _gen.GenerateDungeon();
